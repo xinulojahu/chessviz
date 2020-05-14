d4e680dc8bd9ccab3b15db846c2042058c84ccac
xinulojahu <kashkarev154@gmail.com>
2020-05-15 02:39:56 +0700

    Broke codestyle

--- src/main.c
+++ src/main.c
@@ -4,8 +4,8 @@
 
 int main()
 {
-    char board[8][8];
-    board_init(board);
+		    char board[8][8];
+    board_init( wЖboard);
     board_print_plain(board);
     board_read(board);
     board_print_plain(board);
