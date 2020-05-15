3b038e97efa2a432bc37a6ff810277b8f70b6460
xinulojahu <kashkarev154@gmail.com>
2020-05-16 04:54:47 +0700

    Add board_test func for tests

--- test/board_test.c
+++ test/board_test.c
@@ -1,1 +1,7 @@
+#include "board_test.h"
+#include "board_validation.h"
+#include "ctest.h"
 
+void board_test()
+{
+}
---
+++ test/main.c
@@ -0,0 +1,8 @@
+#include "board_test.h"
+#include <stdio.h>
+
+int main()
+{
+    board_test();
+    return 0;
+}
